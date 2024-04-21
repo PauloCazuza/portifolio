d64784276c7fb712c25ed45a6959716054ae1612
paulo.roberto.lopes <prlopes_@hotmail.com>
2024-04-21 15:18:07 -0300

    fix: change api backend

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const apiService = axios.create({
   // baseURL: "http://localhost:3333",
-  baseURL: "https://api-email-nest-75g5hcuea-paulo-roberto-lopes-projects.vercel.app",
+  baseURL: "https://api-email-nest.vercel.app/",
 });
