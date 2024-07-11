f19697e266e28ed46da7fc7e44d8bafbca7ae4ff
paulo.roberto.lopes <prlopes_@hotmail.com>
2024-07-11 16:49:01 -0300

    fix: color ul transparent

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -9,7 +9,7 @@ function NavBar() {
     const [showHamburger, setShowHamburguer] = useState(false);
     const [colorNavBar, setColorNavBar] = useState("bg-[#E5E5E5]");
     if (typeof window === "undefined")
-        return null;    
+        return null;
     const [linkCurrent, setLinkCurrent] = useState(window.location.pathname);
     const [showModal, setShowModal] = useState(false);
 
@@ -76,7 +76,7 @@ function NavBar() {
                         </button>
                     </div>
                     <div className="items-center justify-between w-full md:flex md:w-auto md:order-1" hidden={!showHamburger} id="navbar-search">
-                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-[#E5E5E5] md:dark:bg-[#E5E5E5] dark:border-gray-700">
+                        <ul style={{ backgroundColor: "transparent" }} className="flex bg-tr flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-[#E5E5E5] md:dark:bg-[#E5E5E5] dark:border-gray-700">
                             <li>
                                 <Link
                                     href="/home"
