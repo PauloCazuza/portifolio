81dd12ad065da87b52d4bb9bd7e8cedb36ee2571
Paulo Roberto Lopes <50050830+PauloCazuza@users.noreply.github.com>
2025-08-26 18:29:36 -0300

    Update index.ts

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const apiService = axios.create({
   // baseURL: "http://localhost:3333",
-  baseURL: "https://api-email-nest.vercel.app/",
+  // baseURL: "https://api-email-nest.vercel.app/",
+  baseURL: "https://backend-portiforio-bnu2b12fs-paulo-roberto-lopes-projects.vercel.app/",
 });
